678b955c1ca30c7c472d6360554b7e7a5677a6d4
Sheikh Sayed <157264560+Sayed1-11@users.noreply.github.com>
2024-10-09 22:40:01 +0600

    Update views.py

--- Turf/views.py
+++ Turf/views.py
@@ -4,13 +4,13 @@ from rest_framework.decorators import action
 from .models import Turf, Sports, TimeSlot, Price,SportField,Facility,SlotEligibility
 from .serializers import TurfSerializer, SportsSerializer, TimeSlotSerializer, PriceSerializer,SportFieldSerializer,FacilitySerializer,SlotEligibilitySerializer
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 class TurfViewSet(viewsets.ModelViewSet):
     queryset = Turf.objects.all()
     serializer_class = TurfSerializer
-
+    permission_classes = [IsAuthenticated | IsAdminUser]
     def get_queryset(self):
-        # Prefetch related fields to optimize database queries
         return Turf.objects.prefetch_related(
             'facilities', 'sports', 'fields', 'fields__prices', 'slot_eligibilities'
         ).all()
@@ -24,6 +24,7 @@ class TurfViewSet(viewsets.ModelViewSet):
     def search(self, request):
         name = request.query_params.get('name')
         location = request.query_params.get('location')
+        sport_name = request.query_params.get('sport_name') 
 
         queryset = self.get_queryset()
         
@@ -31,23 +32,25 @@ class TurfViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(name__icontains=name)
         if location:
             queryset = queryset.filter(location__icontains=location)
+        if sport_name:  # Filter by sport name if provided
+            queryset = queryset.filter(sports__name__icontains=sport_name)
 
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
-# ViewSet for Sports
+
 class SportsViewSet(viewsets.ModelViewSet):
     queryset = Sports.objects.all()
     serializer_class = SportsSerializer
-    
+    permission_classes = [IsAuthenticated | IsAdminUser]
 class FacilitiesViewSet(viewsets.ModelViewSet):
     queryset = Facility.objects.all()
     serializer_class = FacilitySerializer
-
+    permission_classes = [IsAuthenticated | IsAdminUser]
 class SportFieldViewSet(viewsets.ModelViewSet):
     queryset = SportField.objects.all()
     serializer_class = SportFieldSerializer
-
+    permission_classes = [IsAuthenticated | IsAdminUser]
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -61,20 +64,23 @@ class SportFieldViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
         return Response(serializer.data)
-# ViewSet for TimeSlot
+
 class TimeSlotViewSet(viewsets.ModelViewSet):
     queryset = TimeSlot.objects.all()
     serializer_class = TimeSlotSerializer
+    permission_classes = [IsAuthenticated | IsAdminUser]
 class TimeSlotEligibilityViewSet(viewsets.ModelViewSet):
     queryset = SlotEligibility.objects.all()
     serializer_class = SlotEligibilitySerializer
+    permission_classes = [IsAuthenticated | IsAdminUser]
 
-# ViewSet for Price
 class PriceViewSet(viewsets.ModelViewSet):
     queryset = Price.objects.all()
     serializer_class = PriceSerializer
-
+    permission_classes = [IsAuthenticated | IsAdminUser]
+    
 class FieldTypeChoicesView(APIView):
+    permission_classes = [IsAuthenticated | IsAdminUser]
     def get(self, request):
         choices = SportField.FIELD_TYPE_CHOICES
         return Response({'field_types': choices})
