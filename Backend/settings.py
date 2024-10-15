336561f1374d58c38cd6700547763746d2fe3a8e
Sheikh Sayed <157264560+Sayed1-11@users.noreply.github.com>
2024-10-15 23:34:01 +0600

    Update settings.py

--- Backend/settings.py
+++ Backend/settings.py
@@ -107,7 +107,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
+
 
 CHANNEL_LAYERS = {
     'default': {
