d71a23f9f9441e4fafd5fb1ad3097ea09a3b54fb
Sheikh Sayed <157264560+Sayed1-11@users.noreply.github.com>
2024-10-21 19:27:51 +0600

    Add files via upload

---
+++ Slot/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+admin.site.register(TurfSlot)
+admin.site.register(BadmintonSlot)
+admin.site.register(SwimmingSession)
+admin.site.register(SwimmingSlot)
+admin.site.register(SlotHistory)
