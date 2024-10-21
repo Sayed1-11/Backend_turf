557ccb27c24547e1af77c62199aaee020663ff20
Sheikh Sayed <157264560+Sayed1-11@users.noreply.github.com>
2024-10-21 19:25:26 +0600

    Add files via upload

---
+++ Booking/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Turf_Booking,Badminton_Booking,Swimming_Booking
+# Register your models here.
+admin.site.register(Turf_Booking)
+admin.site.register(Badminton_Booking)
+admin.site.register(Swimming_Booking)
