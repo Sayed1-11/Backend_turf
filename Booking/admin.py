e5024385036f12a875074a086f74a0f82846453a
Sheikh Sayed <157264560+Sayed1-11@users.noreply.github.com>
2024-10-21 19:23:43 +0600

    Delete Booking directory

--- Booking/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Turf_Booking,Badminton_Booking,Swimming_Booking
-# Register your models here.
-admin.site.register(Turf_Booking)
-admin.site.register(Badminton_Booking)
-admin.site.register(Swimming_Booking)
