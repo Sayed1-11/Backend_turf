2c3531a6af8a335a9225774841f77ece75eb2c8f
Sheikh Sayed <157264560+Sayed1-11@users.noreply.github.com>
2024-10-25 18:30:52 +0600

    Delete Slot directory

--- Slot/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(TurfSlot)
-admin.site.register(BadmintonSlot)
-admin.site.register(SwimmingSession)
-admin.site.register(SwimmingSlot)
-admin.site.register(SlotHistory)
