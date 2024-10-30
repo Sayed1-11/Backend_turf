39c73807038f37189ed3a54e471b08924f47bf61
Sheikh Sayed <157264560+Sayed1-11@users.noreply.github.com>
2024-10-30 22:21:36 +0600

    Update urls.py

--- Backend/urls.py
+++ Backend/urls.py
@@ -1,3 +1,19 @@
+"""
+URL configuration for Backend project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
@@ -5,7 +21,7 @@ from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 from User.views import UserViewset,UserProfileUpdateViewset,AdminUserSignupViewSet
 from rest_framework.authtoken.views import obtain_auth_token
-from Turf.views import TurfViewSet,SportsViewSet,TimeSlotViewSet,PriceViewSet,SportFieldViewSet,FacilitiesViewSet,TimeSlotEligibilityViewSet,FieldTypeChoicesView,ReviewViewSet
+from Turf.views import TurfViewSet,SportsViewSet,TimeSlotViewSet,PriceViewSet,SportFieldViewSet,FacilitiesViewSet,TimeSlotEligibilityViewSet,FieldTypeChoicesView,ReviewViewSet,ReviewViewSet_perosn
 from Slot.views import BadmintonSlotViewSet,TurfSlotViewSet,SwimmingSessionViewSet,SwimmingSlotViewSet,SlotHistoryViewSet
 from Booking.views import TurfBookingViewSet, BadmintonBookingViewSet, SwimmingBookingViewSet,Booking_history
 from Booking.views import aamarpay_callback, payment_success, payment_failure,MyBookingsViewSet
@@ -15,6 +31,7 @@ router.register(r"user",UserViewset,basename="user")
 router.register(r"user-update",UserProfileUpdateViewset,basename="update")
 router.register(r"turfs", TurfViewSet, basename="turfs")
 router.register(r"reviews", ReviewViewSet, basename="review")
+router.register(r"api/user/review", ReviewViewSet_perosn, basename="user-review")
 router.register(r'sports', SportsViewSet)
 router.register(r'facilities', FacilitiesViewSet)
 router.register(r'My_bookings', MyBookingsViewSet,basename='MyBookings')
