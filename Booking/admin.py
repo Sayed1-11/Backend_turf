ad2fbc87562772bc1e73d2da9e639644934efde7
Sheikh Sayed <157264560+Sayed1-11@users.noreply.github.com>
2024-11-09 17:13:44 +0600

    Delete Booking directory

--- Booking/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Turf_Booking,Badminton_Booking,Swimming_Booking,Booking_History
-# Register your models here.
-admin.site.register(Turf_Booking)
-admin.site.register(Badminton_Booking)
-admin.site.register(Swimming_Booking)
-admin.site.register(Booking_History)
--- Booking/tasks.py
+++
@@ -1,8 +0,0 @@
-from celery import shared_task
-from django.utils import timezone
-from .models import Turf_Booking
-
-@shared_task
-def update_booking_status():
-    # Your task implementation
-    pass
