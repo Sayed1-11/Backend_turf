f5a50106969095737cf7a0a173a3949cd68e841e
Sheikh Sayed <157264560+Sayed1-11@users.noreply.github.com>
2024-11-14 02:33:44 +0600

    Add files via upload

---
+++ Turf/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+admin.site.register(Facility)
+admin.site.register(Sports)
+admin.site.register(TurfAdmin)
+admin.site.register(Favorite)
+# Inline class for SportField to add it directly inside Turf Admin
+class SportFieldInline(admin.TabularInline):
+    model = SportField
+    extra = 1  # Number of empty forms displayed
+    fields = ['field_type', 'sport', 'width', 'height']
+
+# Inline class for SlotEligibility to add it directly inside Turf Admin
+class SlotEligibilityInline(admin.TabularInline):
+    model = SlotEligibility
+    extra = 1
+    fields = ['time_slot', 'is_available', 'reason']
+    
+
+class PriceEligibilityInline(admin.TabularInline):
+    model = Price
+    extra = 1
+    fields = ['field','time_slot', 'price_per_hour','duration_hours','days_of_week']
+
+@admin.register(Turf)
+class TurfAdmin(admin.ModelAdmin):
+    inlines = [SportFieldInline, SlotEligibilityInline,PriceEligibilityInline]
+    list_display = ['name', 'location', 'rating']  
+    search_fields = ['name', 'location'] 
+admin.site.register(TimeSlot)
+admin.site.register(Review)
+admin.site.register(Price)
+admin.site.register(SlotEligibility)
+admin.site.register(SportField)
