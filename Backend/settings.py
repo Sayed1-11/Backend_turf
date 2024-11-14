4a825fb634e6f5f6d56f35cd967ea5a2d2619b41
Sheikh Sayed <157264560+Sayed1-11@users.noreply.github.com>
2024-11-15 00:05:39 +0600

    Update settings.py

--- Backend/settings.py
+++ Backend/settings.py
@@ -126,10 +126,15 @@ MEDIA_URL = '/media/'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.tskqzbcsmnbiypvwivdv',
+        'PASSWORD': 'Sayed19125546',  # Replace with the actual password
+        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
+        'PORT': '6543',
     }
 }
 
